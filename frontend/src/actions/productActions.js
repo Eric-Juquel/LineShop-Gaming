222f52df38ffb6c1205c2b918ceea034213b04fc
Eric J <ejuquel@yahoo.fr>
2020-12-28 18:06:27 +0100

    fix action error '/'

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -3,13 +3,16 @@ import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
   PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants'
 
 export const listProducts = (pageNumber = '') => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
 
-    const { data } = await axios.get(`api/products?pageNumber=${pageNumber}`)
+    const { data } = await axios.get(`/api/products?pageNumber=${pageNumber}`)
 
     dispatch({
       type: PRODUCT_LIST_SUCCESS,
@@ -18,6 +21,27 @@ export const listProducts = (pageNumber = '') => async (dispatch) => {
   } catch (error) {
     dispatch({
       type: PRODUCT_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    })
+  }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+  try {
+    dispatch({ type: PRODUCT_DETAILS_REQUEST })
+
+    const { data } = await axios.get(`/api/products/${id}`)
+
+    dispatch({
+      type: PRODUCT_DETAILS_SUCCESS,
+      payload: data,
+    })
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAILS_FAIL,
       payload:
         error.response && error.response.data.message
           ? error.response.data.message
--- frontend/src/components/content/shop/ProductDetail.js
+++ frontend/src/components/content/shop/ProductDetail.js
@@ -1,12 +1,25 @@
-import React from 'react'
-import classes from './ProductDetail.module.scss'
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import classes from "./ProductDetail.module.scss";
+import Rating from "./Rating";
+import { listProductDetails } from "../../../actions/productActions";
 
-const ProductDetail = () => {
-    return (
-        <div>
-            Product
-        </div>
-    )
-}
+const ProductDetail = ({ match }) => {
+  const dispatch = useDispatch();
 
-export default ProductDetail
+  const productDetails = useSelector(state => state.productDetails)
+  const { loading, error, product} = productDetails
+
+  console.log(match.params)
+
+  useEffect(() => {
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
+
+
+
+  return <div>Product</div>;
+};
+
+export default ProductDetail;
